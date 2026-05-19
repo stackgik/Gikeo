402a72b89dc76afb0f940cae89962bbe73c72702
stackgik <uc@MacBookAir.mshome.net>
2026-05-19 14:47:42 +0100

    add default login details for easy testing

--- src/features/auth/LoginForm.tsx
+++ src/features/auth/LoginForm.tsx
@@ -5,8 +5,8 @@ import { Link } from "react-router-dom";
 
 const LoginForm = (): JSX.Element => {
   const [focusedField, setFocusedField] = useState<string | null>(null);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("examplereach@gmail.com");
+  const [password, setPassword] = useState("Adedoyin1994!");
   const { login, isLoggingIn } = useLogin();
 
   const handleFocus = (field: string) => setFocusedField(field);
